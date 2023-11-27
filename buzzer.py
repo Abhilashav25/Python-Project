b43c748e0100fd4a9579187d08d34d9fc8a2637c
AmulBabe <138318613+AmulBabe@users.noreply.github.com>
2023-11-27 19:00:59 +0530

    Add files via upload

---
+++ buzzer.py
@@ -0,0 +1,11 @@
+from machine import Pin
+from utime import sleep_ms
+buzz = Pin(2,Pin.OUT)
+button_pin = Pin(0,Pin.IN, Pin.PULL_UP)
+while True:
+    if button_pin.value()==0:
+        buzz.on()
+        sleep_ms(200)  
+    else:
+        buzz.off()
+#         sleep_ms(200)
