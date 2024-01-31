7ae3d7f39dab326316bf76f3f2772621083247ff
Abhilasha V <138318613+Abhilashav25@users.noreply.github.com>
2024-01-31 18:44:59 +0530

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -5,7 +5,7 @@ button_pin = Pin(0,Pin.IN, Pin.PULL_UP)
 while True:
     if button_pin.value()==0:
         buzz.on()
-        sleep_ms(200) 
+        sleep_ms(200)
     else:
         buzz.off()
         sleep_ms(200)
