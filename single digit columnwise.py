d03be5dae4ee76c54a6a6b3908e421c9d4a20b1f
Abhilasha V <138318613+Abhilashav25@users.noreply.github.com>
2024-02-15 19:20:25 +0530

    Add files via upload

---
+++ single digit columnwise.py
@@ -0,0 +1,32 @@
+
+from machine import Pin
+from utime import sleep_ms
+import time
+num_rows=7
+num_col=5
+row_pins=[15,2,4,16,17,5,18]
+col_pins=[19,21,22,23,14]
+
+row_pins=[Pin(pin,Pin.OUT) for pin in row_pins]
+col_pins=[Pin(pin,Pin.OUT) for pin in col_pins]
+
+while True:
+    for rp in row_pins:
+        for cp in col_pins:
+            cp.off()
+            rp.on()
+            time.sleep(1)
+            cp.on()
+            rp.off()
+        time.sleep(1)
+    
+    
+
+            
+
+
+
+    
+
+
+
